833d936b9b2d4147be952c9e55b6ee9184526a2c
Igor <Duran.kar90@gmail.com>
2018-02-26 12:29:03 +0200

    List done

--- scripts/indexScripts/click.js
+++ scripts/indexScripts/click.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
        var item = $("#inputItemInList");
        $("#productList").append('<li class="itemList">'+item.val()+'<button class="btn-remove redBackground whiteText" type="button" value="remove">X</button></li>');
        item.val('');
-       $(".doneItemListBtn").show();
-   });
-
+       $("#doneItemListBtn").show();
+});
 });
