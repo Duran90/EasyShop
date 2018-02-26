603f0b234a924e00ee00124c3b7241af7014f205
Igor <Duran.kar90@gmail.com>
2018-02-26 13:29:41 +0200

    done

--- scripts/indexScripts/click.js
+++ scripts/indexScripts/click.js
@@ -2,8 +2,25 @@ $(document).ready(function(){
     console.log("Done");
    $("#addItemInListBtn").click(function () {
        var item = $("#inputItemInList");
-       $("#productList").append('<li class="itemList">'+item.val()+'<button class="btn-remove redBackground whiteText" type="button" value="remove">X</button></li>');
-       item.val('');
-       $("#doneItemListBtn").show();
+       if(!item.val() =="") {
+           $("#productList").append('<li class="itemList">' + item.val() + '<button class="btn-remove redBackground whiteText" type="button" value="remove">X</button></li>');
+           item.val('');
+       }else {
+           alert("Enter the name of the product!")};
+        if($("#productList").children().length > 0){
+            $("#doneItemListBtn").show();
+        }
+       $(".btn-remove").click(function () {
+           $(this).parents(".itemList").remove();
+           if($("#productList").children().length <=0){
+               $("#doneItemListBtn").hide();
+           }
+       })
+
 });
+    $("#doneItemListBtn").click(function () {
+        alert($("#productList").children().length)
+    })
+
+
 });
