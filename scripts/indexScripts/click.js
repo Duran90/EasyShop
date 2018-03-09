f30faa167bba466f7e3b1011c1ffb216f4dff953
Igor <Duran.kar90@gmail.com>
2018-03-09 18:57:06 +0200

    Сделан Json

--- scripts/indexScripts/click.js
+++ scripts/indexScripts/click.js
@@ -1,39 +1,49 @@
-$(document).ready(function(){
-   $("#addItemInListBtn").click(function () {
-       var item = $("#inputItemInList");
-       var amount = $("#inputItemAmount");
-       if (amount.val() == "") {
-           alert("Enter Amount")
-       }else if (!item.val() == "") {
-           $("#productList").append('<li class="itemList" data-amount = \"' + amount.val() + '\">' + item.val()+
-               '<span>'+amount.val()+'</span>' + ' qty' +
-               '<sup><button class="btn-remove redBackground whiteText" type="button" value="remove">X</button></sup></li>');
-           item.val('');
-           amount.val('');
+var recuestJson = {};
+$(document).ready(function () {
+    $("#addItemInListBtn").click(function () {
+        let item = $("#inputItemInList");
+        let amount = $("#inputItemAmount");
+        if (amount.val() == "") {
+            alert("Enter Amount")
+        } else if (!item.val() == "") {
+            $("#productList").append('<li class="itemList" data-amount = \"' + amount.val() + '\" data-value=\"' + item.val() + '\">' + item.val() +
+                '<span>' + amount.val() + '</span>' + ' qty' +
+                '<sup><button class="btn-remove redBackground whiteText" type="button" value="remove">X</button></sup></li>');
+            item.val('');
+            amount.val('');
 
-       } else {
-           alert("Enter the name of the product!")
-       }
-       if ($("#productList").children().length > 0) {
-           $("#doneItemListBtn").show();
-       }
-
-       $(".btn-remove").click(function () {
-           $(this).parents(".itemList").remove();
-           if($("#productList").children().length <=0){
-               $("#doneItemListBtn").hide();
-           }
-       })
-});
-    $("#doneItemListBtn").click(function () {
-        var itemList = $(".itemList");
-        console.log(itemList.val());
-        var doneList =[];
-        for(var i = 0; i < itemList.length; i ++){
-            doneList[i] = itemList.get
+        } else {
+            alert("Enter the name of the product!")
+        }
+        if ($("#productList").children().length > 0) {
+            $("#doneItemListBtn").show();
         }
 
+        $(".btn-remove").click(function () {
+            $(this).parents(".itemList").remove();
+            if ($("#productList").children().length <= 0) {
+                $("#doneItemListBtn").hide();
+            }
+        })
+    });
+    $("#doneItemListBtn").click(function () {
+        let itemsList = $(".itemList");
+        let items = {};
+        let name;
+        let amount;
+        for (let i = 0; i < itemsList.length; i++) {
+            name = itemsList[i].dataset.value;
+            amount = itemsList[i].dataset.amount;
+            items[name] = amount;
+        }
+        recuestJson = {
+            location: {
+                lng: longitude,
+                lat: latitude
+            },
+            radius: 2.0,
+            items
+        };
     })
-
 
 });
---
+++ scripts/indexScripts/navigator.js
@@ -0,0 +1,31 @@
+var latitude;
+var longitude;
+$(document).ready(function () {
+    navigator.geolocation.getCurrentPosition(coordinates, errorMessages);
+});
+
+function coordinates(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+}
+
+function errorMessages(error) {
+    switch (error.code) {
+        //заперт на использование геолокации
+        case error.PERMISSION_DENIED:
+            alert("You have refused to provide your location. Our application may not work correctly!");
+            break;
+        //ошибка определения координат(в основнов в неподдерживаемых браузерах)
+        case error.POSITION_UNAVAILABLE:
+            alert("It's impossible to determine your location.");
+            break;
+        //    Привышен лимит времени
+        case error.TIMEOUT:
+            alert("server timeout exceeded");
+            break;
+        //    неизвестная ошибка
+        case error.UNKNOWN_ERR:
+            alert("Unknown error!")
+    }
+}
+
