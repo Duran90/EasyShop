683850d15a0eda7020224efc49101485d20f18fe
Sergey Lebedev <79271593360@yandex.ru>
2018-04-07 19:27:20 +0300

    правки

--- scripts/cartScripts/map.js
+++ scripts/cartScripts/map.js
@@ -2,7 +2,7 @@ function initMap() {
     console.log('456');
     var coord={lat: latitude, lng: longitude};
     console.log("lat"+latitude+" "+"lng "+ longitude);
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         center: coord,
         zoom: 15
     });
--- scripts/cartScripts/switch.js
+++ scripts/cartScripts/switch.js
@@ -3,9 +3,18 @@ $('#myonoffswitch').change(function () {
     if(this.checked){
         $('#optimal').hide();
         $('#economy').show();
+        //left-side
+        $('#totalOptimal').hide();
+        $('#totalEconomy').show();
+        $('#yourEconomyOpacity').hide();
+        $('#yourEconomy').show();
     } else {
         $('#economy').hide();
         $('#optimal').show();
+        $('#totalEconomy').hide();
+        $('#totalOptimal').show();
+        $('#yourEconomy').hide();
+        $('#yourEconomyOpacity').show();
     }
 
 });
